1e48fec9898994d1f78e353ad9eb54a8a324661f
AmirMorteza <amir81kermankerman@gmail.com>
2023-10-24 11:41:51 +0330

    fix the bug

--- app/api/auth/authOptions.ts
+++ app/api/auth/authOptions.ts
@@ -1,42 +1,37 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@/prisma/generated/client'
 import { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from 'bcrypt';
 
-
-
-
+// Make sure to generate the Prisma client
 const prisma = new PrismaClient();
 
-
 export const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-      CredentialsProvider({
-  name:'Credentials',
-  credentials: {
-    email: {lable: 'Email', type: 'email', placeholder: 'Email' },
-    password: {label: 'Password', type: 'password', placeholder: 'Password' },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'email', placeholder: 'Email' },
+        password: { label: 'Password', type: 'password', placeholder: 'Password' },
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials.password) return null;
+
+        const user = await prisma.user.findUnique({ where: { email: credentials.email } });
+        if (!user) return null;
+        const passwordsMatch = await bcrypt.compare(credentials.password, user.hashedPassword!);
+        return passwordsMatch ? user : null;
+      },
+    }),
+    GithubProvider({
+      clientId: process.env.github_client_id!,
+      clientSecret: process.env.github_client_secret!,
+    }),
+  ],
+  session: {
+    strategy: 'jwt',
   },
-  async authorize(credentials,req) {
-  if (!credentials?.email || !credentials.password) return null;
-  
-  const user = await prisma.user.findUnique({ where: {email: credentials.email},
-  });
-  if (!user) return null;
-  const passwordsMatch = await bcrypt.compare(credentials.password, user.hashedPassword!);
-  return passwordsMatch ? user : null;
-  
-  },
-      }),
-      GithubProvider({
-        clientId: process.env.github_client_id!,
-        clientSecret: process.env.github_client_secret!
-      })
-    ],
-    session: {
-  strategy:'jwt'
-    }
-  };
+};
